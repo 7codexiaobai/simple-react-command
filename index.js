1c656acded516a2933bad1f741f1adb582feac73
zhangjinpeng <zjp01329675@alibaba-inc.com>
2023-04-10 15:17:59 +0800

    fea:添加webpack模版，添加package.json模版

--- index.js
+++ index.js
@@ -17,11 +17,15 @@ const htmlTemplate=`<!DOCTYPE html>
 </body>
 </html>
 <script src="main.js"></script>`;
+
 //main.js模版
-const mainJsTemplate=`import React,{ReactDOM} from "react";
+const mainJsTemplate=`import React from "react";
+import ReactDOM from 'react-dom';
 import App from './App.jsx'
-const root=document.getElementById('root');
-React.createElement(<App />,root);`;
+const root = document.getElementById('root');
+console.log(App);
+ReactDOM.render(<App />,root)`;
+
 //app.js模版
 const appJsTemplate=`import React from "react";
 function App<FunctionComponent>() {
@@ -29,6 +33,87 @@ function App<FunctionComponent>() {
 }
 export default App;`;
 
+//package.json模版配置
+const packageJsonTemplate=`{
+    "devDependencies": {
+      "@babel/core": "^7.21.4",
+      "@babel/preset-env": "^7.21.4",
+      "@babel/preset-react": "^7.18.6",
+      "babel-loader": "^9.1.2",
+      "css-loader": "^6.7.3",
+      "file-loader": "^6.2.0",
+      "friendly-errors-webpack-plugin": "^1.7.0",
+      "html-webpack-plugin": "^5.5.0",
+      "react": "^18.2.0",
+      "style-loader": "^3.3.2",
+      "webpack": "^5.78.0",
+      "webpack-bundle-analyzer": "^4.8.0",
+      "webpack-cli": "^5.0.1",
+      "webpack-dev-server": "^4.13.2"
+    },
+    "scripts": {
+      "start": "npx webpack && npx webpack-dev-server --config webpack.config.js"
+    },
+    "dependencies": {
+      "react-dom": "^18.2.0"
+    }
+  }`;
+
+
+//webpackConfig配置
+const webpackConfigJsTemplate=`const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  mode: 'development', // 模式，可以是development或production
+  entry: './src/index.js', // 入口文件
+  output: { // 输出文件
+    filename: 'bundle.js', // 文件名
+    path: path.resolve(__dirname, 'dist') // 输出目录
+  },
+  module: { // 模块加载器
+    rules: [ // 每个加载器对应一个规则
+      {
+        test: /\.css$/, // 匹配规则
+        use: [ // 加载器
+          'style-loader', // 将CSS注入到HTML中
+          'css-loader' // 加载CSS文件
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env','@babel/preset-react'],
+            }},
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader' // 加载图片文件
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [ // 插件
+    new HtmlWebpackPlugin({ // 自动生成HTML文件
+      template: './src/index.html' // 模板文件
+    }),
+    new FriendlyErrorsWebpackPlugin(),
+    // new BundleAnalyzerPlugin(),
+  ],
+  devServer: { // 开发服务器
+    static: path.join(__dirname, './dist'),
+    port: 8080 // 端口号
+  }
+};`
 program
   .version('0.1.0')
   .command('hello')
@@ -40,19 +125,38 @@ program
     const type=options.type
     console.log(`Hello, ${name}!`);
     console.log(`Hello, ${type}!`);
-    fs.writeFile('index.html',htmlTemplate,(err)=>{
-        console.log(err);
+    //生成public模版
+    fs.mkdir('public',{ recursive: true },(err)=>{
+        if(err){
+            console.log(err);
+        }
+        fs.writeFile('public/index.html',htmlTemplate,(err)=>{
+            if(err){
+                console.log(err);
+            } 
+       });
     })
+    //生成src
     fs.mkdir('src', { recursive: true }, (err) => {
         if (err) throw err;
         console.log('src 目录已创建');
         fs.writeFile('src/App.tsx',appJsTemplate,(err)=>{
             console.log(err);
         })
-        fs.writeFile('src/index.js',mainJsTemplate,(err)=>{
+        fs.writeFile('src/main.js',mainJsTemplate,(err)=>{
             console.log(err);
         })
-      });    
+      });  
+      
+    //生成package
+    fs.writeFile('package.json',packageJsonTemplate,(err)=>{
+        console.log(err);
+    })
+
+    //生成webpack配置
+    fs.writeFile('webpack.config.js',webpackConfigJsTemplate,(err)=>{
+        console.log(err);
+    })
   });
 
 program.parse(process.argv);
